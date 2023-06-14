6ca6f54f9744a3297769fe0056fe426773aac950
swapnil <sk9137587676@gmail.com>
2023-06-14 23:24:34 +0530

    Demo package created..

---
+++ Demo/src/com/softura/Demo.java
@@ -0,0 +1,19 @@
+package com.softura;
+
+import java.util.Arrays;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		String str ="we love java";
+		String sub = str.substring(1,5);
+		String sub1 = str.substring(5);
+		int a=10;
+		String valueOf = String.valueOf(a);
+		System.out.println(valueOf);
+		
+		System.out.println(sub);
+		System.out.println(sub1);
+	}
+
+}
