688bf585612ec7c5d52114b24f83acbda3c12c7a
swapnil <sk9137587676@gmail.com>
2023-06-19 11:57:54 +0530

    spring boot webserver jetty.

---
+++ SpringBoot/SprinBoot-01/src/main/java/com/softura/SprinBoot01Application.java
@@ -0,0 +1,13 @@
+package com.softura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBoot01Application.class, args);
+	}
+
+}
